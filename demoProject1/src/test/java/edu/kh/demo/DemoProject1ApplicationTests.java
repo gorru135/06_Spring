0c0a42241fb18abc0064a917f89974b2e6c2831b
gorru135 <gggorru@gmail.com>
2024-04-03 21:05:23 +0900

    spring

--- demoProject1/src/test/java/edu/kh/demo/DemoProject1ApplicationTests.java
+++ demoProject1/src/test/java/edu/kh/demo/DemoProject1ApplicationTests.java
@@ -1,10 +1,10 @@
-package edu.kh.todo;
+package edu.kh.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demoproject1ApplicationTests {
+class DemoProject1ApplicationTests {
 
 	@Test
 	void contextLoads() {
