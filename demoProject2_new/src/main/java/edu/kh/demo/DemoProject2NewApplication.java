3c3f6a59a1dd4a6fe6c2d9475f2f684a865ab228
gorru135 <gggorru@gmail.com>
2024-04-03 17:22:17 +0900

    first commit

---
+++ demoProject2_new/src/main/java/edu/kh/demo/DemoProject2NewApplication.java
@@ -0,0 +1,13 @@
+package edu.kh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject2NewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject2NewApplication.class, args);
+	}
+
+}
