4e576a3992a1ef0478814d6256c711e69e4bd41b
gorru135 <gggorru@gmail.com>
2024-04-29 18:34:08 +0900

    spring

---
+++ boardProject-boot/src/main/java/edu/kh/project/board/model/service/CommnetServiceImpl.java
@@ -0,0 +1,42 @@
+package edu.kh.project.board.model.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.kh.project.board.model.dto.Comment;
+import edu.kh.project.board.model.mapper.CommentMapper;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+@RequiredArgsConstructor
+public class CommnetServiceImpl implements CommentService{
+
+	private final CommentMapper mapper;
+
+	@Override
+	public List<Comment> select(int boardNo) {
+		// TODO Auto-generated method stub
+		return mapper.select(boardNo);
+	}
+	
+	@Override
+	public int insert(Comment comment) {
+		
+		return mapper.insert(comment);
+	}
+	
+	@Override
+	public int update(Comment comment) {
+		
+		return mapper.update(comment);
+	}
+	
+	@Override
+	public int delete(int commentNo) {
+		
+		return mapper.delete(commentNo);
+	}
+}
